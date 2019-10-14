c7463ac90aeef6273c34f813cdfb823e4ff85cde
explorati <w1748419447@163.com>
2019-10-14 12:19:28 +0800

    使用Stream流的方式 改进集合代码 更简洁、Cool

--- src/test/java/com/kingdie/wangweijian/lambda/stream/StreamVS.java
+++ src/test/java/com/kingdie/wangweijian/lambda/stream/StreamVS.java
@@ -10,6 +10,8 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 /**
  * @ Author : Weijian_Wang
@@ -36,15 +38,15 @@ public class StreamVS {
          * 实现需求一：购物车中都有什么
          */
         System.out.println("购物车中商品有：");
-        System.out.println(JSON.toJSONString(skuList,true));
+        System.out.println(JSON.toJSONString(skuList, true));
 
         /**
          * 实现需求二：图书类商品都买
          */
         List<Sku> notBooksSkuList = new ArrayList<>();
         //将除了图书类的商品放入新建集合notBooksSkuList内
-        for(Sku sku : skuList){
-            if(!sku.getSkuCategory().equals(SkuCategoryEnum.BOOKS)){
+        for (Sku sku : skuList) {
+            if (!sku.getSkuCategory().equals(SkuCategoryEnum.BOOKS)) {
                 notBooksSkuList.add(sku);
             }
         }
@@ -58,9 +60,9 @@ public class StreamVS {
         notBooksSkuList.sort(new Comparator<Sku>() {
             @Override
             public int compare(Sku sku1, Sku sku2) {
-                if(sku1.getTotalPrice() > sku2.getTotalPrice()){
+                if (sku1.getTotalPrice() > sku2.getTotalPrice()) {
                     return -1;
-                } else if(sku1.getTotalPrice() < sku2.getTotalPrice()){
+                } else if (sku1.getTotalPrice() < sku2.getTotalPrice()) {
                     return 1;
                 } else {
                     return 0;
@@ -71,7 +73,7 @@ public class StreamVS {
         //创建一个新的集合，大小为2
         List<Sku> top2SkuList = new ArrayList<Sku>();
         //将除图书类之外的 排好序的商品集合 中的前两位放到新集合中
-        for(int i = 0; i < 2; i ++){
+        for (int i = 0; i < 2; i++) {
             top2SkuList.add(notBooksSkuList.get(i));
         }
 
@@ -80,13 +82,13 @@ public class StreamVS {
          */
         Double money = 0.0;
         //计算总价
-        for(Sku sku : top2SkuList){
+        for (Sku sku : top2SkuList) {
             money += sku.getTotalPrice();
         }
 
         //打印最贵的两件商品名称
         List<String> resultSkuNameList = new ArrayList<>();
-        for(Sku sku : top2SkuList){
+        for (Sku sku : top2SkuList) {
             resultSkuNameList.add(sku.getSkuName());
         }
 
@@ -101,18 +103,18 @@ public class StreamVS {
         PriorityQueue<Sku> pq = new PriorityQueue<>(new Comparator<Sku>() {
             @Override
             public int compare(Sku sku1, Sku sku2) {
-                if (sku1.getTotalPrice() > sku2.getTotalPrice()){
+                if (sku1.getTotalPrice() > sku2.getTotalPrice()) {
                     return -1;
-                } else if (sku1.getTotalPrice() < sku2.getTotalPrice()){
+                } else if (sku1.getTotalPrice() < sku2.getTotalPrice()) {
                     return 1;
-                } else{
+                } else {
                     return 0;
                 }
             }
         });
 
         //将除图书类商品外的所有商品放入最大堆中
-        for(Sku sku : notBooksSkuList){
+        for (Sku sku : notBooksSkuList) {
             pq.add(sku);
         }
 
@@ -121,7 +123,7 @@ public class StreamVS {
 
         //打印最贵两件商品名称
         System.out.print("最贵的两件商品 名称为：");
-        for(int i = 2; i >= 1; i--){
+        for (int i = 2; i >= 1; i--) {
             System.out.print(pq.poll().getSkuName() + "  ");
         }
 
@@ -133,7 +135,46 @@ public class StreamVS {
     /**
      * 以Stream流方式实现需求
      */
+    @Test
     public void newCartHandle() {
+        AtomicReference<Double> money = new AtomicReference<>(Double.valueOf(0.0));
+        List<String> skuList =
+                 CartService.getCartSkuList()
+                        .stream()
+                        /**
+                         * 1.打印商品信息
+                         */
+                        .peek(sku -> System.out.println(JSON.toJSONString(sku, true)))
+                        /**
+                         * 2.过滤掉所有图书类商品
+                         */
+                        .filter(sku -> !SkuCategoryEnum.BOOKS.equals(sku.getSkuCategory()))
+                        /**
+                         * 排序
+                         */
+                        .sorted(Comparator.comparing(Sku::getTotalPrice).reversed())
+                        /**
+                         * TOP2
+                         */
+                        .limit(2)
+                        /**
+                         * 累加商品总金额
+                         */
+                        .peek(sku -> money.set(money.get() + sku.getTotalPrice()))
+                        /**
+                         * 获取商品名称
+                         */
+                        .map(sku -> sku.getSkuName())
+                        /**
+                         * 收集结果
+                         */
+                        .collect(Collectors.toList());
+
+        /**
+         * 打印输出结果
+          */
+        System.out.println(JSON.toJSONString(skuList,true));
+        System.out.println("商品总价：" + money.get());
 
     }
 }
